3789108d846bba639ae089b7199155cea658cc1f
jon-renzil-08 <johnirevormasiziliwu@gmail.com>
2026-05-26 15:36:12 +0800

    install Astro Node adapter for server-side rendering support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
-// https://astro.build/config
 export default defineConfig({
-   output: "server",
+  output: "server",
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
